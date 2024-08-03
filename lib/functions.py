afc5e292306a957824ef3d1340c0b19f0fbc1dc2
Leigh Dotey <leighdotey@gmail.com>
2024-08-03 14:59:42 -0400

    completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
-def greet(name):
+def greet(name="Guido"):
+    print(f"Hello, {name}!")
+    
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
-def add(num1, num2):
+def add(num1=45, num2=55):
+    return num1+num2
     pass
 
 def halve(number):
+    if (number != number): return None
+    return number / 2
     pass
